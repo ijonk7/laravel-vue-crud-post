37a3d97f138e2b254397009286050236f0e77896
ijonk7 <jookeo.rizal@gmail.com>
2021-05-20 22:22:19 +0700

    update edit post

--- resources/js/components/posts/Edit.vue
+++ resources/js/components/posts/Edit.vue
@@ -3,7 +3,7 @@
         <div class="row justify-content-center">
             <div class="col-md-12">
                 <div class="card card-default">
-                    <div class="card-header"><strong>EDIT POST</strong></div>
+                    <div class="card-header"><strong>EDITT POST</strong></div>
                     <div class="card-body">
 
                         <form @submit.prevent="PostUpdate">
