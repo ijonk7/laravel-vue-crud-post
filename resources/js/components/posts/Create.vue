e0d0119246cd42a7f336eb53d3168ce7f6584094
ijonk7 <jookeo.rizal@gmail.com>
2021-05-20 22:17:24 +0700

    update create post

--- resources/js/components/posts/Create.vue
+++ resources/js/components/posts/Create.vue
@@ -3,7 +3,7 @@
         <div class="row justify-content-center">
             <div class="col-md-12">
                 <div class="card card-default">
-                    <div class="card-header"><strong>ADD POST</strong></div>
+                    <div class="card-header"><strong>ADD POSTS</strong></div>
 
                     <div class="card-body">
                         <form @submit.prevent="PostStore">
